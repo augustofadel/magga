683c417db731e0cd24e0fa9b3100b9bfbc161e87
augustofadel <augustofadel@gmail.com>
2018-05-18 17:10:23 -0300

    added option to save progress

--- R/func_microagg_brkga.R
+++ R/func_microagg_brkga.R
@@ -13,9 +13,10 @@ magga <- function
   dissim = NULL,
   vars_select = NULL,
   n_agreg = c(3, 4, 5, 10),
-  metricas = c('IL1'),
+  metricas = c('IL1'), # Quando mais de uma metrica e especificada, a primeira e adotada como funcao objetivo. As demais sao utilizadas apenas como metricas.
   d = 'euclidean',
   pop = NULL,
+  init_method = 'TSP',
   init_path = 'TSP',
   pk = 100,
   tot_ger = 200,
@@ -25,6 +26,7 @@ magga <- function
   crossover = 'regen',
   multik = F,
   nuc = 2,
+  save_progress = F,
   verbose = F
 ) {
 
@@ -52,7 +54,25 @@ magga <- function
       dist(method = d)
   }
   n_obj <- nrow(dat)
-  n_agreg <- sort(n_agreg)
+  n_agreg <- sort(n_agreg) %>% as.list()
+
+  if (save_progress) {
+    progress <-
+      vector('list', length(n_agreg)) %>%
+      lapply(function(x) {
+        list(
+          fitness = vector('list', length(metricas)) %>%
+            lapply(function(y) {matrix(NA, nrow = tot_ger, ncol = pk)}),
+          best = matrix(NA, nrow = n_obj, ncol = tot_ger),
+          t = vector('numeric', tot_ger)
+        )
+      })
+    names(progress) <- n_agreg
+
+    for (i in 1:length(progress)) {
+      names(progress[[i]]$fitness) <- metricas
+    }
+  }
 
 
   # Inicializacao -----------------------------------------------------------
@@ -81,7 +101,6 @@ magga <- function
     }
 
     # construcao populacao inicial
-    n_agreg <- as.list(n_agreg)
     if (make_cl) {
       cl <- parallel::makeCluster(nuc)
     } else {
@@ -100,40 +119,41 @@ magga <- function
     if (make_cl)
       parallel::stopCluster(cl)
     names(pop) <- n_agreg
-    if (multik) {
-      pop <- list(do.call('cbind', pop))
-      sol <- vector('list', 1)
-      names(sol) <- names(pop) <- 'multik'
-    } else {
-      sol <- vector('list', length(n_agreg))
-      names(sol) <- paste0('k=', n_agreg)
-    }
+  }
+
+  if (multik) {
+    pop <- list(do.call('cbind', pop))
+    sol <- vector('list', 1)
+    names(sol) <- names(pop) <- 'multik'
+  } else {
+    sol <- vector('list', length(n_agreg))
+    names(sol) <- paste0('k=', n_agreg)
   }
 
   if (verbose)
     cat('\nCalculando fitness... ')
   if (make_cl) {
     cl <- parallel::makeCluster(nuc)
-    # doParallel::registerDoParallel(cl)
+    doParallel::registerDoParallel(cl)
   }
   for (i in 1:length(pop)) {
     if (make_cl) {
-      # fitness <-
-      #   foreach::foreach(
-      #     j = 1:ncol(pop[[i]]),
-      #     .combine = 'cbind',
-      #     .packages = c('data.table', 'dplyr', 'pdist'),
-      #     .export = c('fit', 'agreg', 'DLD', 'SDID', 'IL1', 'IL2', 'IL3')
-      #   ) %dopar% {
-      #     fit(dat, pop[[i]][ , j], metricas)
-      #   }
-      clusterExport(cl, list('fit', 'dat', 'metricas'))
-      fitness <- parallel::parApply(
-        cl,
-        pop[[i]],
-        2,
-        function(x) {fit(dat, x, metricas)}
-      )
+      fitness <-
+        foreach::foreach(
+          j = 1:ncol(pop[[i]]),
+          .combine = 'cbind',
+          .packages = c('data.table', 'dplyr', 'pdist'),
+          .export = c('fit', 'agreg', 'DLD', 'SDID', 'IL1', 'IL2', 'IL3')
+        ) %dopar% {
+          fit(dat, pop[[i]][ , j], metricas)
+        }
+      # clusterExport(cl, list('fit', 'dat', 'metricas', 'agreg', '%>%', 'IL1', 'IL2', 'DLD'))
+      # fitness <- parallel::parApply(
+      #   cl,
+      #   pop[[i]],
+      #   2,
+      #   function(x) {fit(dat, x, metricas)}
+      # )
     } else {
       fitness <- matrix(NA, ncol = ncol(pop[[i]]), nrow = length(metricas))
       for (j in 1:ncol(pop[[i]])) {
@@ -161,6 +181,7 @@ magga <- function
     if (!verbose)
       pb <- txtProgressBar(min = 0, max = tot_ger, style = 3)
     while (geracao <= tot_ger) {
+      # t_start_g <- Sys.time()
       if (verbose) {
         cat('processando geracao', geracao, '\n')
         cat('   _obtendo conjuntos de solucoes elite e nao elite\n')
@@ -181,7 +202,35 @@ magga <- function
         )
         mutant <- do.call('cbind', mutant)
       } else {
-        mutant <- init_population(n_agreg[[i]], n_obj, size_mutant, input)
+        # mutant <- init_population(n_agreg[[i]], n_obj, size_mutant, input)
+        if (toupper(init_method) == 'KMEANS') {
+          clusterExport(cl, list('kmeans_ma', 'clus_regen', 'n_agreg', 'i', 'dissim'))
+          mutant <- parSapply(
+            cl,
+            1:size_mutant,
+            function(x) kmeans_ma(dat, n_agreg[[i]], dissim = as.matrix(dissim))
+          )
+        } else if (toupper(init_method) == 'TSP') {
+          clusterExport(cl, list('init_population', 'init_tsp', 'n_agreg', 'i', 'dissim'))
+          mutant <- init_population(
+            k = n_agreg[[i]],
+            n_obj = n_obj,
+            p = size_mutant,
+            input = init_tsp(dat, dissim),
+            cl = cl
+          )
+        } else if (toupper(init_method) == 'MST') {
+          clusterExport(cl, list('init_population', 'init_mst', 'n_agreg', 'i', 'dissim'))
+          mutant <- init_population(
+            k = n_agreg[[i]],
+            n_obj = n_obj,
+            p = size_mutant,
+            input = init_mst(dat, dissim),
+            cl = cl
+          )
+        } else {
+          stop('Unknow initialization method.')
+        }
       }
       if (verbose)
         cat('   _executando crossover\n')
@@ -276,17 +325,33 @@ magga <- function
       )
       pop_atual <- pop_atual[, order(pop_atual[n_obj + 1,])]
 
+      # salva progresso
+      if (verbose)
+        cat('   _salvando progresso\n')
+      if (save_progress) {
+        # progress[[i]]$t[geracao] <- as.numeric(Sys.time() - t_start_g)
+        progress[[i]]$t[geracao] <- Sys.time()
+        for (metric in 1:nrow(fitness)) {
+          progress[[i]]$fitness[[metric]][geracao,] <- pop_atual[n_obj + metric,]
+        }
+        progress[[i]]$best[, geracao] <- pop_atual[-(n_obj + 1:length(metric)), 1]
+      }
+
       if (!verbose)
         setTxtProgressBar(pb, geracao)
       geracao <- geracao + 1
     } #end_while
     if (!verbose)
       close(pb)
-    sol[[i]] <- pop_atual[-(n_obj + 1),]
+    sol[[i]] <- pop_atual[-(n_obj + 1:length(metric)),]
     dimnames(sol[[i]]) <- NULL
   } #end_for
   cat('\nConcluido.\n\n\n')
   if (make_cl)
     parallel::stopCluster(cl)
-  return(sol)
+  if (save_progress) {
+    return(list(final_pop = sol, progress = progress))
+  } else {
+    return(sol)
+  }
 }
